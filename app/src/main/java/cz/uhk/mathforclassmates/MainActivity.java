c0c3522fb9ac5b343b487337718495936d2ad410
Anatolius95 <valenta.tomas95@gmail.com>
2016-07-18 10:34:30 +0800

    Design

--- app/src/main/java/cz/uhk/mathforclassmates/MainActivity.java
+++ app/src/main/java/cz/uhk/mathforclassmates/MainActivity.java
@@ -1,7 +1,9 @@
 package cz.uhk.mathforclassmates;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void introduction(View view) {
+        Intent intent = new Intent(this, IntroductionActivity.class);
+        startActivity(intent);
+    }
+
+    public void book(View view) {
+        Intent intent = new Intent(this, BookActivity.class);
+        startActivity(intent);
+    }
+
+    public void eShop(View view) {
+        Intent intent = new Intent(this, EShopActivity.class);
+        startActivity(intent);
+    }
+
+    public void aboutUs(View view) {
+        Intent intent = new Intent(this, AboutUsActivity.class);
+        startActivity(intent);
+    }
+
+    public void contact(View view) {
+        Intent intent = new Intent(this, ContactActivity.class);
+        startActivity(intent);
     }
 }
